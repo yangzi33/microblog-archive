012d6ed16b6e66037551052ecd1b2e466dfc686d
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2021-06-13 15:02:06 -0700

    Chapter 3

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# Note: to initiaize db for the first time, run `flask db init` at basedir
+db = SQLAlchemy(app)
+
+# To migrate, run `flask db migrate -m "<message>"`, followed by `flask db upgrade`
+# for migration update
+migrate = Migrate(app, db)
+
+# 1. Routes
+# 2. Database Models
+from app import routes, models
--- flask/microblog/config.py
+++ flask/microblog/config.py
@@ -9,6 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
-    # Config for SQLite
+    # Configurations for SQLite
+    # Take defined database path or confiure a database named app.db
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
+    # 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
