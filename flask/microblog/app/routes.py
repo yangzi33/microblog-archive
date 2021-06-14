a1e838b9f30fa35bd07b64f0897b93833d48dd49
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2021-06-13 21:26:46 -0700

    User registration

--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_required
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 
 # For user authentication/login/logout
 from flask_login import current_user, login_user, logout_user
@@ -57,6 +57,22 @@ def login():
         
     # Render template
     return render_template("login.html", title = "Sign In", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("User {} is successfully registered.".format(form.username.data))
+        return redirect(url_for("login"))
+    return render_template("register.html", title="register", form=form)
 
 
 # Logout route
