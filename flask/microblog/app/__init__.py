e1b4158ea8e7baab7952a2b5bb72f6e83b96a376
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2022-01-01 22:55:36 -0800

    User profile editing impl

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+# Modules for Error Logging
+import logging
+from logging.handlers import SMTPHandler
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -18,4 +22,58 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
-from app import routes, models
+from app import routes, models, errors
+
+from logging.handlers import RotatingFileHandler
+import os
+
+
+# Log errors by email
+if not app.debug:
+
+    ###################################
+    # Configure Email Error Handling. #
+    ###################################
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        # Configure email handler
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure
+        )
+
+        # Set and add email handler
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    ######################################
+    # Configure logging errors to files. #
+    ######################################
+
+    # Create log dir if not exists
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    # Configure file handler
+    # Rotating File Handler ensures the log files don't grow too large (10KB limit)
+    # when the app runs for a long time.
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+
+    # Set and add file handler
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
+
--- flask/microblog/config.py
+++ flask/microblog/config.py
@@ -6,11 +6,21 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
     # Configurations for SQLite
     # Take defined database path or confiure a database named app.db
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
-    # 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    # Email Configuration
+    # for error handling
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
